e612a4ee349707144b407e65f5f7e8ef5950332b
benthrasher5 <benthrasher5@gmail.com>
2019-05-05 19:46:17 -0500

    fixed some bugs with config file reading

--- src/Mapping/ControllerConverters/include/ControllerConverter.h
+++ src/Mapping/ControllerConverters/include/ControllerConverter.h
@@ -6,7 +6,7 @@
 
 class ControllerConverter {
     public:
-        virtual void convert(controller_t& input_controller, controller_t& output_controller, std::map<std::string, std::string> user_map) = 0;
+        virtual void convert(controller_t& input_controller, controller_t& output_controller) = 0;
 };
 
 #endif
--- src/Mapping/ControllerConverters/include/ControllerConverterFactory.h
+++ src/Mapping/ControllerConverters/include/ControllerConverterFactory.h
@@ -21,18 +21,18 @@ class ControllerConverterFactory
 		//List of supported input/output combinations currently supported
 		if(input_controller.type == XB1) {
 			if (output_controller.type == SNES) {
-				userMap = getMapFromConfigFile(".\\Config\\XboxToSnesConfig.txt");
+				userMap = getMapFromConfigFile("XboxToSnesConfig.txt");
 				return new XboxToSnesControllerConverter(userMap);
 			} else if (output_controller.type == NES) {
-				userMap = getMapFromConfigFile(".\\Config\\XboxToNesConfig.txt");
+				userMap = getMapFromConfigFile("XboxToNesConfig.txt");
 				return new XboxToNesControllerConverter(userMap);
 			}
 		} else if(input_controller.type == PS4) {
 			if (output_controller.type == SNES) {
-				userMap = getMapFromConfigFile(".\\Config\\Ps4ToSnesConfig.txt");
+				userMap = getMapFromConfigFile("Ps4ToSnesConfig.txt");
 				return new Ps4ToSnesControllerConverter(userMap);
 			} else if (output_controller.type == NES) { 
-				userMap = getMapFromConfigFile(".\\Config\\Ps4ToNesConfig.txt");
+				userMap = getMapFromConfigFile("Ps4ToNesConfig.txt");
 				return new Ps4ToNesControllerConverter(userMap);
 			}
 		}
@@ -46,51 +46,6 @@ class ControllerConverterFactory
 		ControllerConverterFactory() {};
 
 		static std::map<std::string, std::string> getMapFromConfigFile(std::string configFileName) {
-			// return {
-			// 	{"A", "B "},
-			// 	{"B", "A "},
-			// 	{"X", "X "},
-			// 	{"Y", "Y "},
-			// 	{"START", "START "},
-			// 	{"SELECT", "SELECT "},
-			// 	{"LB", "LB LT "},
-			// 	{"RB", "RB RT "},
-			// 	{"D_LEFT", "D_LEFT LS_LEFT "},
-			// 	{"D_RIGHT", "D_RIGHT LS_RIGHT "},
-			// 	{"D_UP", "D_UP LS_UP "},
-			// 	{"D_DOWN", "D_DOWN LS_DOWN "},
-			// 	{"TRIGGER_DEADZONE", "100"},
-			// 	{"STICK_DEADZONE", "10000"}
-			// 	//map built on this config: (xbox to snes)
-			// 	// A=A
-			// 	// B=B
-			// 	// X=X
-			// 	// Y=Y
-			// 	// START=START
-			// 	// SELECT=SELECT
-			// 	// HOME=NULL
-			// 	// LS_PRESS=NULL
-			// 	// RS_PRESS=NULL
-			// 	// LB=LB
-			// 	// RB=RB
-			// 	// LT=LB
-			// 	// RT=RB
-			// 	// TRIGGER_DEADZONE=100
-			// 	// D_LEFT=D_LEFT
-			// 	// D_RIGHT=D_RIGHT
-			// 	// D_DOWN=D_DOWN
-			// 	// D_UP=D_UP
-			// 	// LS_LEFT=D_LEFT
-			// 	// LS_RIGHT=D_RIGHT
-			// 	// LS_UP=D_UP
-			// 	// LS_DOWN=D_DOWN
-			// 	// RS_LEFT=NULL
-			// 	// RS_RIGHT=NULL
-			// 	// RS_UP=NULL
-			// 	// RS_DOWN=NULL
-			// 	// STICK_DEADZONE=10000
-			// };
-
 			//create empty map
 			std::map<std::string, std::string> cfgMap = {};
 			string line;
@@ -109,13 +64,13 @@ class ControllerConverterFactory
 						//split line into left and right of =
 						int equalIndex = line.find("=");
 						std::string leftValue = line.substr(0, equalIndex);
-						std::string rightValue = line.substr(equalIndex+1, line.length);
+						std::string rightValue = line.substr(equalIndex+1);
 
 						//ignore null mappings
 						if(rightValue != "NULL") {
 							//special handling for int values, don't want a space on the end
 							if(leftValue == "STICK_DEADZONE" || leftValue == "TRIGGER_DEADZONE") {
-								cfgMap.insert({rightValue, leftValue});
+								cfgMap.insert({leftValue, rightValue});
 							} else {
 								//if right value exists in map already, add left value to it's value with a space in between (handles multiple mappings to one button)
 								//otherwise we haven't read it yet and we need to make a new entry in the map
