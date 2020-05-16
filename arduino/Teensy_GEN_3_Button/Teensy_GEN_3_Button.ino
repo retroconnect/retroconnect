c061b935c6fc3457e7e335b2827891a8ff509e23
DerfJagged <24844708+DerfJagged@users.noreply.github.com>
2020-05-16 18:41:10 -0500

    Change ISR priority
    ISR priority fixes random hiccups every 10 seconds or so

--- arduino/Teensy_GEN_3_Button/Teensy_GEN_3_Button.ino
+++ arduino/Teensy_GEN_3_Button/Teensy_GEN_3_Button.ino
@@ -5,7 +5,7 @@ const byte PIN_3 = 4; // LEFT     PORTA13
 const byte PIN_4 = 5; // RIGHT    PORTD7    
 //const byte PIN_5;   // (5V unused)
 const byte PIN_6 = 6; // FIRE1    PORTD4    
-const byte PIN_7 = 7; // SELECT   PIND2     
+const byte PIN_7 = 7; // SELECT   PORTD2     
 const byte PIN_9 = 8; // FIRE2    PORTD3    
 
 char data[2];
@@ -30,7 +30,8 @@ char data[2];
 */
 
 void setup() {
-  Serial.write("Started program!\n");
+  Serial.begin(115200);
+  Serial.print("Started program!\n");
   pinMode(PIN_1, OUTPUT); //UP    | Z
   pinMode(PIN_2, OUTPUT); //DOWN  | Y
   pinMode(PIN_3, OUTPUT); //LEFT  | X
@@ -47,31 +48,35 @@ void setup() {
   digitalWriteFast(PIN_6, HIGH);
   digitalWriteFast(PIN_9, HIGH);
 
-  Serial1.begin(115200);
+  attachInterrupt(digitalPinToInterrupt(PIN_7), select_isr, CHANGE); //SELECT
+  NVIC_SET_PRIORITY(IRQ_PORTD, 0);
+
+  Serial1.begin(921600);
+}
+
+FASTRUN void select_isr() {
+  if (digitalReadFast(PIN_7)) {
+    //Serial.print("HIGH\n");
+    digitalWriteFast(PIN_3, data[0] & 4);  //LEFT
+    digitalWriteFast(PIN_4, data[0] & 8);  //RIGHT
+    digitalWriteFast(PIN_1, data[0] & 1);  //UP
+    digitalWriteFast(PIN_2, data[0] & 2);  //DOWN
+    digitalWriteFast(PIN_6, data[0] & 16); //B
+    digitalWriteFast(PIN_9, data[0] & 32); //C
+  }
+  else {
+    //Serial.print("LOW\n");
+    digitalWriteFast(PIN_3, LOW);            //Set low to indicate controller connected
+    digitalWriteFast(PIN_4, LOW);            //Set low to indicate controller connected
+    digitalWriteFast(PIN_6, data[0] & 64);  //A
+    digitalWriteFast(PIN_9, data[0] & 128); //START
+  }
 }
 
 FASTRUN void loop() {
-  while (1) {
-    if (digitalReadFast(PIN_7)) {
-      //Serial.print("HIGH\n");
-      digitalWriteFast(PIN_3, data[0] & 4);  //LEFT
-      digitalWriteFast(PIN_4, data[0] & 8);  //RIGHT
-      digitalWriteFast(PIN_1, data[0] & 1);  //UP
-      digitalWriteFast(PIN_2, data[0] & 2);  //DOWN
-      digitalWriteFast(PIN_6, data[0] & 16); //B
-      digitalWriteFast(PIN_9, data[0] & 32); //C
-      
-      while (digitalReadFast(PIN_7)) {}
-      //Serial.print("LOW\n");
-      digitalWriteFast(PIN_3, LOW);            //Set low to indicate controller connected
-      digitalWriteFast(PIN_4, LOW);            //Set low to indicate controller connected
-      digitalWriteFast(PIN_6, data[0] & 64);  //A
-      digitalWriteFast(PIN_9, data[0] & 128); //START
-    }
-  
-    if (Serial1.available() >= 2) {
-      //Serial.print("READ\n");
+  if (Serial1.available() >= 2) {
       Serial1.readBytes(data, 2);
-    }
+      //Serial.println(data[0], BIN);
+      //Serial.println(data[1], BIN);
   }
 }
